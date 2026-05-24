2e08be9c8e502217320e92d46d618e0b7e194471
Sergei_Meranovich <sergei_meranovich@epam.com>
2026-05-24 11:06:53 +0300

    module 2 data

--- module_1/smeranovich_py_shell_job.py
+++ module_1/smeranovich_py_shell_job.py
@@ -1,32 +1,39 @@
 from io import BytesIO
 import pandas as pd
 import boto3
-import os
 import io
-import site
-from importlib import reload
-reload(site)
-import pyarrow
 
-prefix = 'order-service'
+bucket_name = "smeranovich"
+input_prefix = "csv-data/"
+output_prefix = "parquet-data/"
 
-input_loc = "smeranovich/csv-data/" + prefix + ".csv"
-output_loc = "smeranovich/" + prefix + "/"
-
-input_bucket = input_loc.split('/', 1)[0]
-object_key = input_loc.split('/', 1)[1]
-
-
-output_loc_bucket = output_loc.split('/', 1)[0]
-output_loc_prefix = output_loc.split('/', 1)[1]
-
-
-s3 = boto3.client('s3')
-obj = s3.get_object(Bucket=input_bucket, Key=object_key)
-df = pd.read_csv(io.BytesIO(obj['Body'].read()))
-
-
-parquet_buffer = BytesIO()
+s3_client = boto3.client('s3')
 s3_resource = boto3.resource('s3')
-df.to_parquet(parquet_buffer, index=False)
-s3_resource.Object(output_loc_bucket, output_loc_prefix +  'data' + '.parquet').put(Body=parquet_buffer.getvalue())
+
+response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=input_prefix)
+
+if 'Contents' not in response:
+    print("No files found in", input_prefix)
+else:
+    for obj in response['Contents']:
+        key = obj['Key']
+        
+        if not key.lower().endswith('.csv'):
+            continue
+        
+        # csv-data/order-service.csv → order-service
+        file_name = key.split('/')[-1]
+        name_without_ext = file_name.rsplit('.', 1)[0]
+        
+        csv_obj = s3_client.get_object(Bucket=bucket_name, Key=key)
+        df = pd.read_csv(io.BytesIO(csv_obj['Body'].read()))
+        
+        parquet_buffer = BytesIO()
+        df.to_parquet(parquet_buffer, index=False)
+        
+        output_key = f"{output_prefix}{name_without_ext}/data.parquet"
+        s3_resource.Object(bucket_name, output_key).put(Body=parquet_buffer.getvalue())
+        
+        print(f"Converted: {key} → s3://{bucket_name}/{output_key} ({len(df)} rows)")
+
+print("Done!")
---
+++ module_2/smeranovich_metrics.sql
@@ -0,0 +1,26 @@
+CREATE EXTERNAL TABLE `smeranovich_metrics`(
+  `fromtimestamp` string COMMENT '', 
+  `maxvalue` double COMMENT '', 
+  `metricname` string COMMENT '', 
+  `minvalue` double COMMENT '', 
+  `totimestamp` string COMMENT '', 
+  `unit` string COMMENT '')
+PARTITIONED BY ( 
+  `componentname` string COMMENT '')
+ROW FORMAT SERDE 
+  'org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe' 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat'
+LOCATION
+  's3://smeranovich/metrics'
+TBLPROPERTIES (
+  'classification'='parquet', 
+  'transient_lastDdlTime'='1779605881')
+  
+ALTER TABLE smeranovich_metrics
+    ADD PARTITION (componentname = 'order-service') LOCATION 's3://smeranovich/parquet-data/order-service/';
+	
+ALTER TABLE smeranovich_metrics
+    ADD PARTITION (componentname = 'user-service') LOCATION 's3://smeranovich/parquet-data/user-service/';	
